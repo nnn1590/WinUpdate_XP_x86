82e5f29342830cfdaaf87997035e96f2a8e62f33
NNN1590 <nnn1590@nnn1590.org>
2021-08-02 02:00:52 +0900

    Fix build.sh

--- build.sh
+++ build.sh
@@ -8,8 +8,8 @@ for _PREFIX in 'x86_64-w64-mingw32-' 'i686-w64-mingw32-'; do
 	#[[ "${_PREFIX}" == 'x86_64-w64-mingw32-' ]] && continue  # Skip x86_64 build
 	#[[ "${_PREFIX}" == 'i686-w64-mingw32-' ]] && continue  # Skip x86 build
 	"${_PREFIX}g++${_SUFFIX}" -dumpspecs | sed -e 's/-lmsvcrt/-lmsvcr120/g' > "${_PREFIX}g++${_SUFFIX}.specs"
-	[[ "${_PREFIX}" == 'x86_64-w64-mingw32-' ]] && _OUTPUT="updlist_x86_64${_SUFFIX}.2.exe" || _OUTPUT="updlist_x86${_SUFFIX}.exe"
+	[[ "${_PREFIX}" == 'x86_64-w64-mingw32-' ]] && _OUTPUT="updlist_x86_64${_SUFFIX}.exe" || _OUTPUT="updlist_x86${_SUFFIX}.exe"
 	"${_PREFIX}windres" updlist.rc --output-format=coff -o "${_PREFIX}updlist${_SUFFIX}.res"
-	"${_PREFIX}g++${_SUFFIX}" -specs="${_PREFIX}g++${_SUFFIX}.specs" -DUNICODE -D_UNICODE -mwin32 -municode --static -static-libgcc -static-libstdc++ $(grep RelativePath updlist.vcproj | grep -o '".*"' | grep -vE '(ReadMe.txt|.rc|.ico)"' | sed -ze 's/".\\//g; s/\"\n/ /g') "${_PREFIX}updlist${_SUFFIX}.res" -o "${_OUTPUT}" -lversion -lshlwapi -lcrypt32
+	"${_PREFIX}g++${_SUFFIX}" -specs="${_PREFIX}g++${_SUFFIX}.specs" -DUNICODE -D_UNICODE -mwin32 -municode --static -static-libgcc -static-libstdc++ $(grep RelativePath updlist.vcproj | grep -o '".*"' | grep -vE '(ReadMe.txt|.rc|.ico)"' | sed -ze 's/".\\//g; s/\"\n/ /g') "${_PREFIX}updlist${_SUFFIX}.res" -o "${_OUTPUT}" -lversion -lshlwapi -lcrypt32 -lmsvcr120
 done
 #wait
