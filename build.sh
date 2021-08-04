9799b79aae2f0d4c01a90ce46d6a88077f6d3436
NNN1590 <nnn1590@nnn1590.org>
2021-08-04 16:28:28 +0900

    Exclude header files from g++ and rename BUILD.ubuntu-mingw.md to BUILDING.ubuntu-mingw.md

--- build.sh
+++ build.sh
@@ -10,6 +10,6 @@ for _PREFIX in 'x86_64-w64-mingw32-' 'i686-w64-mingw32-'; do
 	"${_PREFIX}g++${_SUFFIX}" -dumpspecs | sed -e 's/-lmsvcrt/-lmsvcr120/g' > "${_PREFIX}g++${_SUFFIX}.specs"
 	[[ "${_PREFIX}" == 'x86_64-w64-mingw32-' ]] && _OUTPUT="updlist_x86_64${_SUFFIX}.exe" || _OUTPUT="updlist_x86${_SUFFIX}.exe"
 	"${_PREFIX}windres" updlist.rc --output-format=coff -o "${_PREFIX}updlist${_SUFFIX}.res"
-	"${_PREFIX}g++${_SUFFIX}" -specs="${_PREFIX}g++${_SUFFIX}.specs" -DUNICODE -D_UNICODE -municode --static -static-libgcc -static-libstdc++ $(grep RelativePath updlist.vcproj | grep -o '".*"' | grep -vE '(ReadMe.txt|.rc|.ico)"' | sed -ze 's/".\\//g; s/\"\n/ /g') "${_PREFIX}updlist${_SUFFIX}.res" -o "${_OUTPUT}" -lversion -lshlwapi -lcrypt32 -lmsvcr120
+	"${_PREFIX}g++${_SUFFIX}" -specs="${_PREFIX}g++${_SUFFIX}.specs" -DUNICODE -D_UNICODE -municode --static -static-libgcc -static-libstdc++ $(grep RelativePath updlist.vcproj | grep -o '".*"' | grep -vE '(ReadMe.txt|.rc|.ico|.h)"' | sed -ze 's/".\\//g; s/\"\n/ /g') "${_PREFIX}updlist${_SUFFIX}.res" -o "${_OUTPUT}" -lversion -lshlwapi -lcrypt32 -lmsvcr120
 done
 #wait
